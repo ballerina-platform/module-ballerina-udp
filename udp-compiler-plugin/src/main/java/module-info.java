8bcfea8e16473cd0c1a91820cb59619a4f6a441b
MohamedSabthar <sabtharugc0@gmail.com>
2021-03-18 17:04:24 +0530

    Configure compiler plugin

---
+++ udp-compiler-plugin/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module  io.ballerina.stdlib.graphql.compiler {
+    requires io.ballerina.parser;
+    requires io.ballerina.lang;
+    requires io.ballerina.tools.api;
+    requires io.ballerina.stdlib.udp;
+}
